4a8cdb17623e941938616da712892357cd644b53
lz <Gaurav Chaudhary>
2017-11-28 18:21:03 +0530

    resto Controller added

---
+++ app/Http/Controllers/Backend/RestaurantController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use Illuminate\Http\Request;
+
+class RestaurantController extends Controller
+{
+    public function index(){
+
+        return view('');
+    }
+}
