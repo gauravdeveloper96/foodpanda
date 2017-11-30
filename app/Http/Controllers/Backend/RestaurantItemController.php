0615a3cab5d879fde2b9a29d78658110bff40147
Gaurav Chaudhary <gauravchaudhary.luminoguru@gmail.com>
2017-11-30 11:51:18 +0530

    3 new files added RestaurantItem -model -controller and migration

---
+++ app/Http/Controllers/Backend/RestaurantItemController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Session;
+use App\Models\RestaurantItem;
+use Illuminate\Http\Request;
+
+class RestaurantItemController extends Controller
+{
+    //
+}
