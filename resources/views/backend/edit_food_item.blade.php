cb424858bf3a36ebc786606509ed879141be0fe0
rashmeen <rashmeenkaur.luminoguru@gmail.com>
2017-12-01 11:49:02 +0530

    Edit form created

--- resources/views/backend/edit_food_item.blade.php
+++ resources/views/backend/edit_food_item.blade.php
@@ -8,27 +8,27 @@
 @endsection
 @section('content')
 <div class="form-add-category">
-    {{Form::model($restroDetail, ['route' => ['admin.restaurants.update', $restroDetail->id], 'files'=>true])}}
+    {{Form::model($restroItemsDetail, ['route' => ['admin.restaurantitems.update', $restroItemsDetail->id], 'files'=>true])}}
     {{csrf_field()}}
-        {{ method_field('PUT') }}
-  
+    {{ method_field('PUT') }}
+
     <div class="labels col-sm-2">
-         
+
 
         {{Form::label('item-name', 'Item Name', ['class' => 'display-text'])}}<br>
-         {{Form::label('price', 'Item Price', ['class' => 'display-text'])}}<br>
-        
+        {{Form::label('price', 'Item Price', ['class' => 'display-text'])}}<br>
+
 
     </div>
     <div class="text-fields col-sm-10">      
-        {{Form::text('item-name','',['class' => 'enter-text'])}}<br>
-       
-        {{Form::number('price','',['class' => 'enter-text'])}}
-       
+        {{Form::text('item-name',ucwords($restroItemsDetail->item_name), array('class' => 'enter-text'))}}<br>
+
+        {{Form::number('price',ucwords($restroItemsDetail->price), array('class' => 'enter-text'))}}
+
 
     </div>
-    <a href="#">{{Form::submit('Update Food Item',['class'=>'add-category'])}}
-    </a>
+    {{Form::submit('Update Food Item',['class'=>'add-category'])}}
+
     {{ Form::close() }}
 </div>
 @endsection
