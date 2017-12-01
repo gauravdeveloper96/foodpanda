13f4ef7638288542929a5c3a4752ee8447aa2102
rashmeen <rashmeenkaur.luminoguru@gmail.com>
2017-12-01 13:02:05 +0530

    Hidden fields added

--- resources/views/backend/edit_food_item.blade.php
+++ resources/views/backend/edit_food_item.blade.php
@@ -14,14 +14,17 @@
     {{ method_field('PUT') }}
 
     <div class="labels col-sm-2">
+        {{Form::label('category_id', 'Category Id', ['class' => 'display-text disable'])}}<br>
 
-
+        {{Form::label('restro_id', 'Restro Id', ['class' => 'display-text disable'])}}<br>
         {{Form::label('item-name', 'Item Name', ['class' => 'display-text'])}}<br>
         {{Form::label('price', 'Item Price', ['class' => 'display-text'])}}<br>
 
 
     </div>
-    <div class="text-fields col-sm-10">      
+    <div class="text-fields col-sm-10"> 
+         {{Form::number('category_id',$restroItemsDetail->category_id, array('class' => 'enter-text disable'))}}<br>
+        {{Form::number('restro_id',$restroItemsDetail->restaurant_id, array('class' => 'enter-text disable'))}}<br>
         {{Form::text('item-name',ucwords($restroItemsDetail->item_name), array('class' => 'enter-text'))}}<br>
 
         {{Form::number('price',$restroItemsDetail->price, array('class' => 'enter-text'))}}
