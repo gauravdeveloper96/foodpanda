66444e6ad702e0434c704610ce087eabc00192be
Gaurav Chaudhary <gauravchaudhary.luminoguru@gmail.com>
2017-12-14 16:42:08 +0530

    Merge origin/master
    Conflicts:
    	resources/views/frontend/RestaurantMenu.blade.php

--- app/Http/Controllers/Frontend/User/CartController.php
+++ app/Http/Controllers/Frontend/User/CartController.php
@@ -27,14 +27,15 @@ class CartController extends Controller
             return back();
         }
         if(Auth::check()){
-            
+
             $item =Item::where('id',$item_id)
                 ->select('id','name','restaurant_id','category_id','price')
                 ->first();
-            
+            $quant=1;
+            $item=  array_push($item,$quant );
 //            $item['price']=$item['price']*4;
 
-            
+
             if(empty(session('addToCart'))){
                 session()->put('addToCart',[$item]);
                  $tprice= $item['price'];
@@ -43,15 +44,16 @@ class CartController extends Controller
             }
             else {
                 session()->push('addToCart',$item);
+//                dd(session('addToCart'));
                $tprice=0;
                 foreach (session('addToCart') as $total){
                     $tprice+=$total['price'];
                 }
-               
+
                 return json_encode(session('addToCart'), $tprice);
-                
+
             }
-            
+
         }
         else
             return response()->json(['route' => route('login')],500);
